863c57504c6dddfebb03af8c8dbda08058bd432e
Ahemd Bappy <bappy3a@gmail.com>
2024-03-16 22:59:28 +0600

    todo add done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import Todo from './Todo'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Todo from './Todo';
 
 export default function TodoList() {
+    const todos = useSelector((state) => state.todos);
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-
-            <Todo />
-
-
+            {todos.map(todo => (
+                <Todo todo={todo} kay={todo.id} />
+            ))}
         </div>
     )
 }
